cb2470f40e33b8df25e7b45ee721d82faa4089b8
suarezschou <98514958+suarezschou@users.noreply.github.com>
2022-11-24 22:04:03 +0100

    ingredients
    -cleaned up file struc
    -ingredietes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,67 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{useEffect,useState} from "react";
+import Recipe from './recipe';
+import './App.css';
+
+const App = () => {
+
+  const APP_ID = "af0418f0";
+  const APP_KEY = "0b232f021ed22a658278dcc5ead31f5f";
+  
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query,setQuery] =useState('chicken');
+
+  
+  useEffect(() =>{
+    getRecipes();
+    console.log("effect only runs 1 time now");
+  },[query]);
+  
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+    
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search);
+  }
+
+  return(
+    <div className="App">
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} ></input>
+          <button className="search-button" type="submit">
+            fesf 
+          </button>
+      </form>
+      {recipes.map(recipe =>(
+        <Recipe 
+        key={recipe.recipe.label} // to avoid re-rendering the whole thing we assing a key value
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+    </div>
+  );
+
+};
+
+export default App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +69,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/recipe.js
+++ src/recipe.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 //pass down the props into the component.... essentlially state->props->component
-const Recipe = ({title,calories,image}) =>{
+const Recipe = ({title,calories,image,ingredients}) =>{
     return(
         <div>
             <h1>{title}</h1>
+            <ol>
+                {ingredients.map(ingredients =>(
+                    <li>
+                        {ingredients.text}
+                    </li>
+                ))}
+            </ol>
             <p>{calories}</p>
-            <img src={image} alt=''></img>
+            <img src={image} alt='the dish'></img>
         </div>
     );
 }
